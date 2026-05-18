8ef67cd69c70717db310abfabef54deb86250350
ManavShrivastava103 <manav.shrivastava@avantika.edu.in>
2026-05-18 17:40:26 +0530

    updated file upload controllers

--- controller/uploadfileController.js
+++ controller/uploadfileController.js
@@ -23,7 +23,61 @@ const upload_single_file = async_handler(async(req, res) => {
     res.status(200).json({message: "File Uploaded Successfully", data : uploaded_file});
 });
 
-module.exports = { upload_single_file };
+
+// Function for uploading single file to project
+// POST -> /projects/proj_files/bulk/:project_id
+// access : PRIVATE -> For users with permission UPDATE-PROJECTS
+const bulk_upload_files = async_handler(async(req, res) => {
+    const req_project = await Project.findById(req.params.project_id);
+    if (!req_project) {
+        res.status(404);
+        throw new Error("Project Not Found");
+    }
+    const bulk_files = [];
+    for (const a_file of req.files) {
+        const uploaded_file = await Projfile.create({
+            public_id: a_file.filename,
+            url: a_file.path,
+            file_name: a_file.originalname,
+            file_type: a_file.mimetype,
+            uploaded_by: req.user.user_id,
+            in_project: req.params.project_id
+        });
+        bulk_files.push(uploaded_file);
+    }
+    res.status(200).json({message: "Files Uploaded Successfully", data: bulk_files });
+});
+
+
+// Function for fetching single file to project
+// GET -> /projects/proj_files/:project_file_id
+// access : PRIVATE -> For users with permission UPDATE-PROJECTS
+const get_a_file = async_handler(async(req, res) => {
+    const req_project = await Project.findById(req.params.project_id);
+    if (!req_project) {
+        res.status(404);
+        throw new Error("Project Not Found");
+    }
+    const uploaded_file = await Projfile.findById(req.params.project_file_id);
+    res.status(200).json(uploaded_file);
+});
+
+
+// Function for fetching single file to project
+// GET -> /projects/proj_files/:project_ids
+// access : PRIVATE -> For users with permission UPDATE-PROJECTS
+const get_all_project_files = async_handler(async(req, res) => {
+    const req_project = await Project.findById(req.params.project_id);
+    if (!req_project) {
+        res.status(404);
+        throw new Error("Project Not Found");
+    }
+    const uploaded_files = await Projfile.find({$match: {in_project : req.params.project_id}});
+    res.status(200).json(uploaded_file);
+});
+
+
+module.exports = { upload_single_file, bulk_upload_files };
 
 
 
